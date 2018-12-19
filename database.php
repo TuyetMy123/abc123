f048ba861367b0296ed36091743f14b9851e3fb4
TuyetMy123 <33411058+TuyetMy123@users.noreply.github.com>
2018-12-20 03:46:18 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$server = 'local';
-$username = 'root';
-$password = 'root';
-$database = 'auth';
+$server = 'adminmysql.mysql.database.azure.com';
+$username = 'adminadmin';
+$password = '@abc123@';
+$database = 'adminmysql';
 
 try{
 	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
